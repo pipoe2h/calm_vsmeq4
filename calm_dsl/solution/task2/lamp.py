d04b166d76cedb0d0dfb8faa0982dae84d45e728
Jose Gomez <pipoe2h@gmail.com>
2020-06-12 09:21:35 +0100

    Update: Fix DSL lab

--- calm_dsl/solution/task2/lamp.py
+++ calm_dsl/solution/task2/lamp.py
@@ -1,23 +1,25 @@
 from calm.dsl.builtins import SimpleDeployment, SimpleBlueprint
 from calm.dsl.builtins import read_local_file, basic_cred
-from calm.dsl.builtins import vm_disk_package, AhvVmResources, AhvVmDisk, AhvVmNic, AhvVmGC, AhvVm
+from calm.dsl.builtins import AhvVmResources, AhvVmDisk, AhvVmNic, AhvVmGC, AhvVm
 from calm.dsl.builtins import action, CalmTask, CalmVariable
 
+
+# Change values based on your calm environment
+IMAGE_NAME = 'centos-7'
+NETWORK_NAME = 'External'
+
+# Password file located under './.local'
 CENTOS_PASSWD = read_local_file('centos')
 CENTOS_CRED = basic_cred('centos', CENTOS_PASSWD, name='CENTOS_CRED', default=True)
 
-# OS Image details for VM
-CENTOS_IMAGE_SOURCE = 'https://cloud.centos.org/centos/8/x86_64/images/CentOS-8-ec2-8.1.1911-20200113.3.x86_64.qcow2'
-CentosPackage = vm_disk_package( name='centos_disk', 
-                                 config={'image': {'source': CENTOS_IMAGE_SOURCE}})
 
 class CentosVmResources(AhvVmResources):
 
     memory = 4
     vCPUs = 2
     cores_per_vCPU = 1
-    disks = [AhvVmDisk.Disk.Scsi.cloneFromVMDiskPackage(CentosPackage, bootable=True)]
-    nics = [AhvVmNic.DirectNic.ingress("Network-01")]
+    disks = [AhvVmDisk.Disk.Scsi.cloneFromImageService(IMAGE_NAME, bootable=True)]
+    nics = [AhvVmNic.DirectNic.ingress(NETWORK_NAME)]
     guest_customization = AhvVmGC.CloudInit(
         config={
             'password': CENTOS_PASSWD,
@@ -61,7 +63,7 @@ class LAMPBlueprint(SimpleBlueprint):
     credentials = [CENTOS_CRED]
     deployments = [ApachePHP, HAProxy, MySQL]
 
-    MYSQL_PASSWORD = CalmVariable.Simple.Secret('MYSQL_PASSWORD', label='MySQL root password', runtime=True)
+    MYSQL_PASSWORD = CalmVariable.Simple.Secret('MYSQL_PASSWORD', label='MySQL root password',runtime=True)
     COUNT = CalmVariable.WithOptions.Predefined.string(['1', '2', '3'], default='1', name='COUNT',
                                                         label='Apache Count', runtime=True)
 
